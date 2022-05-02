48aac73d5d6e55cbbedaa54bf2e9bd550ef554cb
Trang1234567 <82450720+Trang1234567@users.noreply.github.com>
2022-05-02 23:26:54 +0200

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,24 @@
+
+function changelisttostaff(){
+    location.href = 'stafflist.html';
+}
+
+function changelisttostudent() {
+    location.href = 'adminPage.html';
+}
+
+function addStudent(){
+    location.href = 'studentform.html';
+}
+
+function addStaff(){
+    location.href = 'staffform.html';
+}
+
+function returntolist() {
+    location.href = 'adminPage.html';
+}
+
+
+
+
